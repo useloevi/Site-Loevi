d4024291039e6f5a103be6879044f8e94a84cc5b
useloevi <useloevi@gmail.com>
2026-08-03 04:05:15 -0300

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Cover from "../components/Cover";
+import IntroSection from "../components/IntroSection";
+import ProductRow1 from "../components/ProductRow1";
+
+export default function Home() {
+  return (
+    <main>
+      <Cover />
+      <IntroSection />
+      <ProductRow1 />
+    </main>
+  );
+}
