8fa51fd5f547cab04ba8a56b37849522588130b1
useloevi <useloevi@gmail.com>
2026-08-13 19:11:17 -0300

    Update price for Choker Florença product
    Updated the price of the 'Choker Florença' product from 'Esgotado' to 'R$ 43,80'.

--- components/ProductGrid.jsx
+++ components/ProductGrid.jsx
@@ -8,7 +8,7 @@ import "./ProductRow.css";
 const products = [
   ["001", "Choker Rivieira Prata", "Colar banhado a prata com pedrinhas de zircônia", "Esgotado"],
   ["002", "Colar Navete Dourado", "Colar banhado a ouro com pedrinhas de zircônia", "R$ 49,00"],
-  ["003", "Choker Florença", "Colar banhado a prata", "Esgotado"],
+  ["003", "Choker Florença", "Colar banhado a prata", "R$ 43,80"],
   ["004", "Choker Amendoada", "Colar banhado a prata", "R$ 41,80"],
   ["005", "Choker Elos", "Colar banhado a prata com elos quadrados", "R$ 37,80"],
   ["006", "Colar V Cristais", "Colar banhado a prata com cristais em zircônia", "R$ 37,80"],
